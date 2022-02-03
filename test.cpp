1cd15a1fbfc6a16c979940932cd6f6ad1e9281bc
Imilkowski <58135969+Imilkowski@users.noreply.github.com>
2022-02-03 16:52:49 +0100

    v1.0
    Pierwsza wersja kodu programu

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+	int halfAmplitude = 32;
+	int sleepTime = 1;
+	float power = 0.05;
+	
+	float i = 0;
+	do{
+		int line = (round(halfAmplitude*sin(i) + halfAmplitude + 1));
+        
+        for(int x = 0; x < line; x++){
+				cout<<"O";
+			}
+		
+		int theRest = (halfAmplitude * 2) + 1;
+		
+		for(int x = 0; x < theRest - line; x++){
+				cout<<".";
+			}
+        
+        cout<<endl;
+        Sleep(sleepTime);
+        
+        i += power;
+	} while(true);
+	
+	system("PAUSE");
+	return 0;
+}
